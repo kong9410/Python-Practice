e443e209d67e8a80d90eee46a218dc1a0a95f60f
unknown <gnitsgogo@gmail.com>
2019-08-17 09:55:03 +0900

    textrank readme

--- sentiment_analysis/progbar.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-with open('text.json', encoding="UTF-8") as json_file:
-    json_data = json.loads(json_file)
-    json_string = json_data["BYLINE"]
-    print(json_data)
-
